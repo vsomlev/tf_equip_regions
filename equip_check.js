ea7fcb5d20a2dbc02b594da65edd0ef28436a61c
Vesso Somlev <v.somlev@gmail.com>
2015-04-12 02:31:54 +0200

    First version of the GUI. Using jQuery: - Autocomplete when typing item name - Displaying image of the selected items - Display info about occuring item conflicts

--- equip_check.js
+++ equip_check.js
@@ -1,67 +1,9 @@
-// If you don't want to use jQuery
-function GET(path, success, error){
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function()
-	{
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				if (success)
-					success(xhr.responseText);
-			} else {
-				if (error)
-					error(xhr);
-			}
-		}
-	};
-	xhr.open("GET", path, true);
-	xhr.send();
-};
-
-// Doesn't work ATM because of Access-Control-Allow-Origin.
-function fetch_client_schema(){
-	var vdf_data = null;
-	var client_schema_location = "http://git.optf2.com/schema-tracking/plain/Team%20Fortress%202%20Client%20Schema?h=teamfortress2";
-	GET(client_schema_location,
-			function(data) {process_equip_data(data)},
-			function(xhr) {alert('Could not fetch the client schema');}
-	   );
-};
-
-function process_equip_data(vdf_text){
-	data = vdf.parse(vdf_text);
-	vdf_text = vdf.stringify(data);
-	console.log(vdf_text);
-};
-
 function fetch_item_list(done_cb){
-	item_list_json_url = 'equip_regions_list.txt';
-	// GET(item_list_json_url,
-	// 		done_cb,
-	// 		function(xhr) { alert('NOPE'); }
-	// );
 	$.ajax({
-		url: item_list_json_url,
+		url: 'equip_regions_list.txt',
 		success: done_cb,
-		error: function(hue) {alert('NOPE');}
+		error: function(x) {alert('Failed loading the items description. Refresh and try again.');}
 	});
-};
-
-// Only for testing, wont be used for production.
-function find_item(partial_name, item_list){
-	partial_name = partial_name.toLowerCase().replace(/\s+/g, '').replace(/'/g, '');
-	// 1. Full match
-	for(var name in item_list){
-		if(name == partial_name){
-			return item_list[name];
-		}
-	}
-	// 2. Partial match
-	for(var name in item_list){
-		if(name.indexOf(partial_name)>-1){
-			return item_list[name];
-		}
-	}
-	return null;
 };
 
 //////////////////////
@@ -76,13 +18,15 @@ conflicts_table = {
 };
 
 function has_conflict(itema, itemb){
-	console.log('---------------------');
-	console.log('Comparing ' + itema['name'] + ' vs ' + itemb['name']);
+	if(typeof itema == 'undefined' || typeof itemb == 'undefined'){
+		return false;
+	}
+	// console.log('---------------------');
+	// console.log('Comparing ' + itema['name'] + ' vs ' + itemb['name']);
 	eqa = itema['equip_regions'];
 	eqb = itemb['equip_regions'];
-	// eqb.push('medal'); // for testing
 
-	console.log('[' + eqa + '] vs [' + eqb + ']');
+	// console.log('[' + eqa + '] vs [' + eqb + ']');
 
 	// 1. Simple checks first
 	has_simple_conflict = eqa.some(function(x){return eqb.indexOf(x)>-1});
