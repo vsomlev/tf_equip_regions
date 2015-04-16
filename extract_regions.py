1d2f2ff863b5b96058f1070608bc622edcbcda43
Vesso Somlev <v.somlev@gmail.com>
2015-04-17 01:34:40 +0200

    Moved all equip_region resolution into pre-processing (extract_regions.py), greatly simplifying the JavaScript code that checks for conflicts (it's literally one line). As a result, equip_check.js is no longer necessary. Various improvements to the python script, massively reducing the size of the items data file.

--- extract_regions.py
+++ extract_regions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2
 import vdf
 import json
-from pprint import pprint
 
 client_schema_file = 'schema_client.vdf'
 main_schema_file = 'schema_main.json'
@@ -9,8 +8,14 @@ main_schema_file = 'schema_main.json'
 ignore_item_classes = ['tool', 'supply_crate']
 accept_item_classes = ['tf_wearable', 'tf_weapon_medigun', 'tf_powerup_bottle']
 
+# does 'whole head' conflict with 'lenses' by transitivity?
+conflicts_table = {
+	'glasses' : ['face', 'lenses'],
+	'whole_head' : ['hat', 'face', 'glasses', 'lenses'],
+}
+
 def prop(dic, prop_name):
-    """ Gets the name property and list-ifies it. """
+    """ Gets name property and set-ifies it. """
     if prop_name in dic: 
         prop = dic[prop_name]
         if isinstance(prop, str): 
@@ -32,31 +37,40 @@ def parse_schema():
 
         # Parsing the items
         items_tmp = {}
-        items = {}
-
         for obj_id, obj in client_schema['items_game']['items'].iteritems():
             item = {}
             item_name = obj['name']
 
+            """
+                Short strings are used for the fields (saving ~32KiB):
+                c - classes
+                e - equip_regions
+                i - image
+            """
+
             # Character Classes
-            item['classes'] = set()
-            item['classes'] |= prop(obj, 'used_by_classes')
+            item['c'] = prop(obj, 'used_by_classes')
 
             # Equip regions
-            item['equip_regions'] = set()
-            item['equip_regions'] |= prop(obj, 'equip_regions')
-            item['equip_regions'] |= prop(obj, 'equip_region')
+            equip_regions = set()
+            equip_regions |= prop(obj, 'equip_regions')
+            equip_regions |= prop(obj, 'equip_region')
 
-            # Prefab also affects the equip regions
-            # for each of the item's prefabs, get that
-            # prefab's equip regions and add them to the item's
+            # Prefab also affects the equip regions.
+            # We already parsed the prefabs earlier.
+            # For each of the item's prefabs, get that
+            # prefab's equip regions and add them to the item's.
             if 'prefab' in obj:
                 item_prefabs = set(obj['prefab'].split(' '))
-                prefab_equip_regions = set()
                 for ipf in item_prefabs:
-                    prefab_equip_regions |= prop(schema_prefabs, ipf)
-                item['equip_regions'] |= prefab_equip_regions
-
+                    equip_regions |= prop(schema_prefabs, ipf)
+                
+            # add equip_regions from the conflicts_table dict
+            item['e'] = set(equip_regions)
+            for eq in equip_regions:
+                if eq in conflicts_table:
+                    item['e'] |= set(conflicts_table[eq])
+                    
             items_tmp[item_name] = item
 
         """
@@ -67,25 +81,29 @@ def parse_schema():
             an unfamiliar internal name. The name which is put here is instead the one
             people see in-game.
         """
-
+        items = {}
         schema = json.loads(main_schema.read())
         schema_items = schema['result']['items']
         for schema_item in schema_items:
             # if schema_item['item_class'] in ignore_item_classes: continue
             if schema_item['item_class'] not in accept_item_classes: continue
-            name = schema_item['name']
-            ingame_name = schema_item['item_name']
-            item = items_tmp[name]
+            
+            # item is refered in both schemas by its internal 'name'
+            item = items_tmp[schema_item['name']]
 
             # sometimes the classes list is only present in the main schema
-            item['classes'] |= prop(schema_item, 'used_by_classes')
-            if len(item['classes'])==0 or len(item['classes'])>8:
-                item['classes'] = ['all']
-
-            item['name'] = ingame_name
-            item['image'] = schema_item['image_url']
-
+            item['c'] |= prop(schema_item, 'used_by_classes')
+            if len(item['c'])==9:
+                item['c'] = []
+                # deleting this elemnt saves only ~2KiB
+            
+            # the common image location prefix is 45 characters long (saving ~65KiB)
+            # http://media.steampowered.com/apps/440/icons/
+            item['i'] = schema_item['image_url'][45:]
+            # the actual in-game name of the item
+            ingame_name = schema_item['item_name']
             items[ingame_name] = item
+        
 
         return items
 
